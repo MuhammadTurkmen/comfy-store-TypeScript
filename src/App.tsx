bcdfb971400846d96133ca00eb80d14da9b5dc03
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-02 11:54:44 +0430

    adding cart.ts

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import { Button } from "@/components/ui/button";
 function App() {
   return (
     <div className="text-4xl p-4">
-      <Button variant="destructive" size="lg">
+      <Button
+        variant="destructive"
+        size="lg"
+        onClick={() => console.log("it worked")}
+      >
         Click me
       </Button>
     </div>
