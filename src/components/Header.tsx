a6e65470a1616cd91aae858664f33ec5694e5eb9
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-04 09:55:18 +0430

    adding classes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,12 +8,24 @@ function Header() {
   });
 
   const handleLogout = () => {
-    console.log("logout");
+    setUser(null);
   };
 
   return (
     <header>
-      <div className="align-element flex justify-center sm:justify-end py-2"></div>
+      <div className="align-element flex justify-center sm:justify-end py-2">
+        {/* USER */}
+        {user ? (
+          <div className="flex gap-x-2 sm:gap-x-8 items-center">
+            <p className=" text-xs sm:text-sm">Hello, {user.username} </p>
+            <Button variant="link" size="sm" onClick={handleLogout}>
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <div className="flex gap-x-6 justify-center items-center -mr-4"></div>
+        )}
+      </div>
     </header>
   );
 }
