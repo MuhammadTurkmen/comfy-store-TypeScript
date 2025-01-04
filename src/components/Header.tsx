6bc661232470a4bb0678ec0a836c8afbaba288ab
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-04 09:41:59 +0430

    setting up header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,20 @@
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
+import { useState } from "react";
+
 function Header() {
-  return <div>Header</div>;
+  const [user, setUser] = useState<{ username: string } | null>({
+    username: "demo user",
+  });
+
+  const handleLogout = () => {
+    console.log("logout");
+  };
+
+  return (
+    <header>
+      <div className="align-element flex justify-center sm:justify-end py-2"></div>
+    </header>
+  );
 }
 export default Header;
