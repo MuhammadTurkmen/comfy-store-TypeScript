ab6ebbdc26d1580eac7fe10ed5145203b4a390c5
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-05 08:41:36 +0430

    adding dropdown

--- src/components/LinksDropdown.tsx
+++ src/components/LinksDropdown.tsx
@@ -1,3 +1,14 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { AlignLeft } from "lucide-react";
+import { Button } from "./ui/button";
+import { links } from "@/utils";
+import { NavLink } from "react-router-dom";
+
 function LinksDropdown() {
   return <div>LinksDropdown</div>;
 }
