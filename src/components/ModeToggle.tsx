a4e2d95db58bdd2d46884fac4633483847f77b30
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-06 11:58:49 +0430

    adding theme

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,3 +1,14 @@
+import { useAppDispatch } from "@/hooks";
+import { Moon, Sun } from "lucide-react";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { setTheme } from "@/featuers/theme/themeSlice";
+
 function ModeToggle() {
   return <div>ModeToggle</div>;
 }
