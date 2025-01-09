50654f473cd886281499b25b8e6dcbeacef5a9bc
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-09 16:04:53 +0430

    making error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -9,7 +9,12 @@ function Error() {
       <main className="grid min-h-[100vh] place-items-center px-8">
         <div className="text-center">
           <p className="text-9xl font-semibold text-primary">404</p>
-          <h1 className="mt-4 text-3xl font-bold"></h1>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
+            page not found
+          </h1>
+          <p className="mt-6 text-lg leading-7">
+            Sorry, we could not find the page you are looking for.
+          </p>
         </div>
       </main>
     );
