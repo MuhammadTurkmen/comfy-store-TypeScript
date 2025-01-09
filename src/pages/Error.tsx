d2faa98dae10e7fddee3ff7ecbeced368282cb32
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-09 16:01:10 +0430

    making error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -2,6 +2,18 @@ import { useRouteError, Link, isRouteErrorResponse } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 
 function Error() {
+  const error = useRouteError();
+  console.log();
+  if (isRouteErrorResponse(error) && error.status === 404) {
+    return (
+      <main className="grid min-h-[100vh] place-items-center px-8">
+        <div className="text-center">
+          <p className="text-9xl font-semibold text-primary">404</p>
+          <h1 className="mt-4 text-3xl font-bold"></h1>
+        </div>
+      </main>
+    );
+  }
   return <div>Error</div>;
 }
 export default Error;
