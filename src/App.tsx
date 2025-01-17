77316672d5f176d1ce5cec549d4309a2224663c0
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-17 11:29:06 +0430

    adding loader

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import {
   SingleProduct,
 } from "./pages";
 import { ErrorElement } from "./components";
+import { loader as landingLoader } from "./pages/Landing";
 
 const router = createBrowserRouter([
   {
@@ -23,7 +24,8 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Landing />,
-        errorElement: <ErrorElement />
+        errorElement: <ErrorElement />,
+        loader: landingLoader
       },
       {
         path: "products",
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,13 @@
 
 import { Hero, FeaturedProdocts } from "@/components";
+import { useLoaderData, type LoaderFunction } from "react-router-dom";
+import { customFetch, type ProductsResponse } from "@/utils";
+
+export const loader: LoaderFunction = async () => {
+  console.log('landing page');
+  return null 
+}
+
 function Landing() {
   return <>
   <Hero />
