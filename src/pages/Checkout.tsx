fdee8d23617a78f8e54096724d5238068e86406d
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-17 08:23:02 +0430

    adding if

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -6,6 +6,10 @@ import { toast } from "@/hooks/use-toast";
 
 function Checkout() {
   const cartTotal = useAppSelector((state) => state.cartState.cartTotal);
+  if (cartTotal === 0) {
+    return <SectionTitle text="Your Cart is empty" />;
+  }
+
   return <div>Checkout</div>;
 }
 export default Checkout;
