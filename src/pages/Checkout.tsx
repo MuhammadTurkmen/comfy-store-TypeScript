2fc602933ae30b7b5386e4cafbb20cf7094a6efd
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-17 08:10:13 +0430

    adding import

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,3 +1,9 @@
+import { useAppSelector } from "@/hooks";
+import { CheckoutForm, SectionTitle, CartTotals } from "@/components";
+import { LoaderFunction, redirect } from "react-router-dom";
+import { type ReduxStore } from "@/store";
+import { toast } from "@/hooks/use-toast";
+
 function Checkout() {
   return <div>Checkout</div>;
 }
