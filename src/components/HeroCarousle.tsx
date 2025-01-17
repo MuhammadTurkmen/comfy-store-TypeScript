11a1b980a7a0e2861dcf696528786701475a570f
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-17 13:01:09 +0430

    adding images

--- src/components/HeroCarousle.tsx
+++ src/components/HeroCarousle.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
+
+import hero1 from "../assets/hero1.webp";
+import hero2 from "../assets/hero2.webp";
+import hero3 from "../assets/hero3.webp";
+import hero4 from "../assets/hero4.webp";
+
+
+const carouselImages = [hero1, hero2, hero3, hero4]
+
 
 function HeroCarousle() {
   return (
-    <div>HeroCarousle</div>
+    <div className="hidden lg:block">
+      <Carousel>
+        <CardContent>
+          {carouselImages.map((image, index) => {
+            return <CarouselItem key={index}>
+              <Card>
+                <CardContent>
+                  <img src={image} alt="hero" className="w-full h-[24rem] rounded-md object-cover" />
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          })}
+        </CardContent>
+      </Carousel>
+    </div>
   )
 }
 
