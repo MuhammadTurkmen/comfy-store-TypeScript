1fc6827d07b347e4fa6da2176a4ffe2d8ddfe3da
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-18 19:31:36 +0430

    adding link

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -3,11 +3,12 @@ import { NavLink } from "react-router-dom";
 import { useAppSelector } from "@/hooks";
 
 function NavLinks() {
-  const user = useAppSelector((state) => state.userState.user)
+  const user = useAppSelector((state) => state.userState.user);
   return (
     <div className="hidden lg:flex justify-center items-center gap-x-4">
       {links.map((link) => {
-        const restrictedRoutes = 
+        const restrictedRoutes =
+          link.href === "chechout" || link.href === "orders";
         return (
           <NavLink
             to={link.href}
