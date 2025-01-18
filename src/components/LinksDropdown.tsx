071baadffc1e8557ce51188bf500508f9a27d7c3
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-18 19:37:15 +0430

    adding useAppSelector

--- src/components/LinksDropdown.tsx
+++ src/components/LinksDropdown.tsx
@@ -8,8 +8,10 @@ import { AlignLeft } from "lucide-react";
 import { Button } from "./ui/button";
 import { links } from "@/utils";
 import { NavLink } from "react-router-dom";
+import { useAppSelector } from "@/hooks";
 
 function LinksDropdown() {
+  const user = useAppSelector((state) => state.userState.user);
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild className="lg:hidden">
