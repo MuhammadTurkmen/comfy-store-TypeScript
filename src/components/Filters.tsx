2ee0b932ae40486e5042d91f9711be4a66ab5740
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-18 12:32:13 +0430

    adding formInput

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,6 +1,4 @@
 import { Form, useLoaderData, Link } from "react-router-dom";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Button } from "./ui/button";
 import { ProductsResponseWithParams } from "@/utils";
 
@@ -10,10 +8,6 @@ function Filters() {
 
   return (
     <Form className="border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
-      <div className="mb-2">
-        <Label htmlFor="search">Search Product</Label>
-        <Input id="search" name="search" type="text" defaultValue={search} />
-      </div>
       <Button type="submit" size="sm" className="self-end mb-2">
         Search
       </Button>
