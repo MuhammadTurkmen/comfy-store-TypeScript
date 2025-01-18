bd132ffa061b366f81d7286608cd85602330ec39
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-18 18:56:49 +0430

    adding restricted route

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,10 +1,13 @@
 import { links } from "@/utils";
 import { NavLink } from "react-router-dom";
+import { useAppSelector } from "@/hooks";
 
 function NavLinks() {
+  const user = useAppSelector((state) => state.userState.user)
   return (
     <div className="hidden lg:flex justify-center items-center gap-x-4">
       {links.map((link) => {
+        const restrictedRoutes = 
         return (
           <NavLink
             to={link.href}
