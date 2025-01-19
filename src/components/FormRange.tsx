dd977a0c1089f152e8bdcffea4ee89eb67e42606
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-19 09:15:46 +0430

    adding import

--- src/components/FormRange.tsx
+++ src/components/FormRange.tsx
@@ -1,3 +1,8 @@
+import { formatAsDollars } from "@/utils";
+import { useState } from "react";
+import { Label } from "./ui/label";
+import { Slider } from "./ui/slider";
+
 function FormRange() {
   return <div>FormRange</div>;
 }
