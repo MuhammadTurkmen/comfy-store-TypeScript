cd7b5db17086bf2c51d6b088ef679077fb444681
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-19 09:11:26 +0430

    adding input

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -2,7 +2,7 @@ import { Form, useLoaderData, Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import { ProductsResponseWithParams } from "@/utils";
 import FormInput from "./FormInput";
-import { FormSelect } from ".";
+import { FormRange, FormSelect } from ".";
 
 function Filters() {
   const { meta, params } = useLoaderData() as ProductsResponseWithParams;
