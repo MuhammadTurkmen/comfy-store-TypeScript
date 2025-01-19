7ec0b3287e1d74e26d3a95c601c688ede7c3768e
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-19 09:32:28 +0430

    adding input

--- src/components/FormRange.tsx
+++ src/components/FormRange.tsx
@@ -3,8 +3,23 @@ import { useState } from "react";
 import { Label } from "./ui/label";
 import { Slider } from "./ui/slider";
 
-function FormRange() {
-  return <div>FormRange</div>;
+type FormRangeProps = {
+  name: string;
+  label?: string;
+  defaltValue?: string;
+};
+
+function FormRange({ name, label, defaultValue }: FormRangeProps) {
+  const step = 1000;
+  const maxPrice = 100000;
+  const defaultPrice = defaultValue ? Number(defaultValue) : maxPrice;
+  const [selectedPrice, setSelectedPrice] = useState(defaultPrice);
+
+  return (
+    <div className="mb-2">
+      <Label htmlFor={name} className="capitalize flex justify-between"></Label>
+    </div>
+  );
 }
 
 export default FormRange;
