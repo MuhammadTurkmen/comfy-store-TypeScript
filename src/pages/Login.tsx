729e7b4a30e55722f943493541a881d2cfeda27a
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-20 11:17:30 +0430

    adding card

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,13 +9,22 @@ import { Card, CardHeader, CardContent, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { SubmitBtn, FormInput } from "@/components";
 import { customFetch } from "@/utils";
-import { toast } from "@/components/ui/use-toast";
+import { toast } from "@/hooks/use-toast";
 import { type ReduxStore } from "@/store";
-import { loginUser } from "@/features/user/userSlice";
+
 import { useAppDispatch } from "@/hooks";
 import { AxiosResponse } from "axios";
+import { loginUser } from "@/featuers/user/userSlice";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <section className="h-screen grid place-items-center">
+      <Card className="w-96 bg-muted">
+        <CardHeader>
+          <CardTitle className="text-center">Login</CardTitle>
+        </CardHeader>
+      </Card>
+    </section>
+  );
 }
 export default Login;
