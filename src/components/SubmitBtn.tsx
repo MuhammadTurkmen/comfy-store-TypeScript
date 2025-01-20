706c987b5ee08845521929b4a1dbbb774694987b
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-20 10:46:23 +0430

    adding text

--- src/components/SubmitBtn.tsx
+++ src/components/SubmitBtn.tsx
@@ -6,14 +6,15 @@ function SubmitBtn({ text, className }: { text: string; className?: string }) {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
   return (
-    <Button
-      type="submit"
-      className={className}
-      disabled={isSubmitting}
-    >
-      {isSubmitting  ? <span className="flex">
-        <ReloadIcon className="mr-2 h-4 w-4 animate-spin"/>
-      </span>}
+    <Button type="submit" className={className} disabled={isSubmitting}>
+      {isSubmitting ? (
+        <span className="flex">
+          <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+          Submitting...
+        </span>
+      ) : (
+        text
+      )}
     </Button>
   );
 }
