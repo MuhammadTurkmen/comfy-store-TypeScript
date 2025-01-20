8ae0d41661c2f61cde9a4b4e20ffbee83a0167f8
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-20 15:22:57 +0430

    adding btn

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -17,6 +17,10 @@ import { AxiosResponse } from "axios";
 import { loginUser } from "@/featuers/user/userSlice";
 
 function Login() {
+  const loginAsGuestUser = () => {
+    console.log("guest user");
+  };
+
   return (
     <section className="h-screen grid place-items-center">
       <Card className="w-96 bg-muted">
@@ -28,6 +32,20 @@ function Login() {
             <FormInput type="email" label="email" name="identifier" />
             <FormInput type="password" name="password" />
             <SubmitBtn text="Login" className="w-full mt-4" />
+            <Button
+              type="button"
+              variant={"outline"}
+              onClick={loginAsGuestUser}
+              className="w-full mt-4"
+            >
+              Guest User
+            </Button>
+            <p className="text-center mt-4">
+              Not a member yet{" "}
+              <Button type="button" asChild variant={"link"}>
+                <Link to={"/register"}>Register</Link>
+              </Button>
+            </p>
           </Form>
         </CardContent>
       </Card>
