cba9b436294396a68fb6dc2677352386ee43a679
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-20 10:37:27 +0430

    adding useNavigation

--- src/components/SubmitBtn.tsx
+++ src/components/SubmitBtn.tsx
@@ -1,4 +1,10 @@
-function SubmitBtn() {
+import { useNavigation } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { ReloadIcon } from "@radix-ui/react-icons";
+
+function SubmitBtn({ text, className }: { text: string; className?: string }) {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
   return <div>SubmitBtn</div>;
 }
 
