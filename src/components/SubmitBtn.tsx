84c87df380d46e77d411d9c5894667f4e052c6dd
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-20 10:41:45 +0430

    adding submitting

--- src/components/SubmitBtn.tsx
+++ src/components/SubmitBtn.tsx
@@ -5,7 +5,13 @@ import { ReloadIcon } from "@radix-ui/react-icons";
 function SubmitBtn({ text, className }: { text: string; className?: string }) {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
-  return <Button type="submit" className={className}></Button>;
+  return (
+    <Button
+      type="submit"
+      className={className}
+      disabled={isSubmitting}
+    ></Button>
+  );
 }
 
 export default SubmitBtn;
