2a918ab807e0dd58c86ffaefad950750e1ea4f7a
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-22 08:25:02 +0430

    adding toest

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,3 +1,11 @@
+import { ActionFunction, Form, Link, redirect } from "react-router-dom";
+import { Card, CardHeader, CardContent, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { SubmitBtn, FormInput } from "@/components";
+import { customFetch } from "@/utils";
+import { toast } from "@/hooks/use-toast";
+import { AxiosError } from "axios";
+
 function Register() {
   return <div>Register</div>;
 }
