3baa777413781e9c71719de7cb79a774295c32ff
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-23 20:20:13 +0430

    adding userState

--- src/featuers/user/userSlice.ts
+++ src/featuers/user/userSlice.ts
@@ -1,4 +1,14 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { toast } from "@/hooks/use-toast";
+
+export type User = {
+  username: string;
+  jwt: string;
+};
+
+type USerState = {
+  user: User | null;
+};
 
 const initialState = {
   name: "user slice",
