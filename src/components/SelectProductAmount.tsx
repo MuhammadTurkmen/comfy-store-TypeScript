1d483219796e0fb4466444b0777db862c4d6f5b0
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-23 08:33:51 +0430

    adding enum

--- src/components/SelectProductAmount.tsx
+++ src/components/SelectProductAmount.tsx
@@ -6,6 +6,11 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
+export enum Mode {
+  SingleProduct = "singleProduct",
+  CartItem = "cartItem",
+}
+
 function SelectProductAmount() {
   return <div>SelectProductAmount</div>;
 }
