078826aac06fa589a29398be3db75aab566bcebc
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-23 10:21:20 +0430

    adding select

--- src/components/SelectProductAmount.tsx
+++ src/components/SelectProductAmount.tsx
@@ -11,8 +11,34 @@ export enum Mode {
   CartItem = "cartItem",
 }
 
-function SelectProductAmount() {
-  return <div>SelectProductAmount</div>;
+type SelectProductAmountProps = {
+  mode: Mode.SingleProduct;
+  amount: number;
+  setAmount: React.Dispatch<React.SetStateAction<number>>;
+};
+
+type SelectCartItemAmountProps = {
+  mode: Mode.CartItem;
+  amount: number;
+  setAmount: (value: number) => void;
+};
+
+function SelectProductAmount({
+  mode,
+  amount,
+  setAmount,
+}: SelectProductAmountProps | SelectCartItemAmountProps) {
+  const cartItem = mode === Mode.CartItem;
+
+  return (
+    <>
+      <h4 className="font-medium mb-2 ">Amount : </h4>
+      <Select
+        defaultValue={amount.toString()}
+        onValueChange={(value) => setAmount(Number(value))}
+      ></Select>
+    </>
+  );
 }
 
 export default SelectProductAmount;
