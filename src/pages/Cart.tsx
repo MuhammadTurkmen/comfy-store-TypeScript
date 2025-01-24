8f3cb31c7dec6176aae0315efec99111b748148e
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-24 12:31:40 +0430

    adding return

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,5 +1,10 @@
 import { useAppSelector } from "@/hooks";
-import { CartItemsList, SectionTitle, CartTotals } from "@/components";
+import {
+  CartItemsList,
+  SectionTitle,
+  CartTotals,
+  CartItemList,
+} from "@/components";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 
@@ -11,10 +16,19 @@ function Cart() {
     (state) => state.cartState.numItemsInCart
   );
 
+  if (numItemsInCart === 0) {
+    return <SectionTitle text="Empty Cart" />;
+  }
+
   return (
-    <div>
-      <h1 className="text-4xl">Cart Page</h1>;
-    </div>
+    <>
+      <SectionTitle text="Shopping Cart" />
+      <div className="mt-8 grid gap-8 lg:grid-cols-12">
+        <div className="lg:col-span-8">
+          <CartItemList />
+        </div>
+      </div>
+    </>
   );
 }
 export default Cart;
