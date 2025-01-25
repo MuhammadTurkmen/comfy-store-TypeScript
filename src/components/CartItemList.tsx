fa6b9a51955341c0486a718428dc4c70d0ec262a
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-25 09:55:07 +0430

    adding props

--- src/components/CartItemList.tsx
+++ src/components/CartItemList.tsx
@@ -21,11 +21,12 @@ function CartItemList() {
             className="flex flex-col gap-y-4 sm:flex-row flex-wrap p-6 mb-8"
           >
             <FirstColumn image={image} title={title} />
-            <FirstColumn
+            <SecondColumn
               title={title}
               company={company}
               productColor={productColor}
             />
+            <ThirdColumn amount={amount} cartID={cartID} />
           </Card>
         );
       })}
