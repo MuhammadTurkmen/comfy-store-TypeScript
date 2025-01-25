38aa0eaffefa38c83fb3784033f286d59bb23e5c
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-25 08:27:42 +0430

    adding imports

--- src/components/CartTotals.tsx
+++ src/components/CartTotals.tsx
@@ -1,3 +1,8 @@
+import { useAppSelector } from "@/hooks";
+import { formatAsDollars } from "@/utils";
+import { Card, CardTitle } from "@/components/ui/card";
+import { Separator } from "./ui/separator";
+
 function CartTotals() {
   return <div>CartTotals</div>;
 }
