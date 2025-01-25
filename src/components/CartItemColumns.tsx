0e2cb6738765798bd82b694f570d3ea80f31c234
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-25 09:43:24 +0430

    adding func

--- src/components/CartItemColumns.tsx
+++ src/components/CartItemColumns.tsx
@@ -6,8 +6,15 @@ import SelectProductAmount from "./SelectProductAmount";
 import { Mode } from "./SelectProductAmount";
 import { editItem, removeItem } from "@/featuers/cart/cartSlice";
 
-function CartItemColumns() {
-  return <div>CartItemColumns</div>;
-}
-
-export default CartItemColumns;
+export const FirstColumn = () => {
+  return <h4>First Column</h4>;
+};
+export const SecondColumn = () => {
+  return <h4>Second Column</h4>;
+};
+export const ThirdColumn = () => {
+  return <h4>Third Column</h4>;
+};
+export const FourthColumn = () => {
+  return <h4>Fourth Column</h4>;
+};
