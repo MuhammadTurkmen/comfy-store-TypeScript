9e25323c147660585456f6197845da5ac769405d
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-25 09:27:07 +0430

    adding classses

--- src/components/CartItemList.tsx
+++ src/components/CartItemList.tsx
@@ -1,5 +1,23 @@
+import { useAppSelector } from "@/hooks";
+import { Card } from "./ui/card";
+
 function CartItemList() {
-  return <div>CartItemList</div>;
+  const cartItems = useAppSelector((state) => state.cartState.cartItems);
+
+  return (
+    <div>
+      {cartItems.map((cartItem) => {
+        return (
+          <Card
+            key={cartItem.cartID}
+            className="flex flex-col gap-y-4 sm:flex-row flex-wrap p-6 mb-8"
+          >
+            Cart Item
+          </Card>
+        );
+      })}
+    </div>
+  );
 }
 
 export default CartItemList;
