eb463397d7a8332fe9b9171f8bb7496764decf03
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-25 10:51:48 +0430

    adding style

--- src/components/CartItemColumns.tsx
+++ src/components/CartItemColumns.tsx
@@ -34,6 +34,17 @@ export const SecondColumn = ({
     <div className="sm:ml-4 md:ml-12 sm:w-48">
       <h3 className="capitalize font-medium">{title}</h3>
       <h4 className="mt-2 capitalize text-sm">{company}</h4>
+      <p className="mt-4 text-sm capitalize flex items-center gap-x-2">
+        color :{" "}
+        <span
+          style={{
+            width: "15px",
+            height: "15px",
+            borderRadius: "50%",
+            background: productColor,
+          }}
+        ></span>
+      </p>
     </div>
   );
 };
