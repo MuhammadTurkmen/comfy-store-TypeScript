1868806d9cded368ae7e120aaec13ccb4692edb1
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-26 10:24:44 +0430

    adding text

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -8,7 +8,11 @@ import { toast } from "@/hooks/use-toast";
 import { clearCart } from "@/featuers/cart/cartSlice";
 
 function CheckoutForm() {
-  return <div>CheckoutForm</div>;
+  return (
+    <Form method="post" className="flex flex-col gap-y-4">
+      <h4 className="font-medium text-xl mb-4">Shipping Information</h4>
+    </Form>
+  );
 }
 
 export default CheckoutForm;
