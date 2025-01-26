fbed41e20bf25601220275d3f96957c4044c66a8
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-26 10:18:13 +0430

    adding import

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -2,9 +2,10 @@ import { ActionFunction, Form, redirect } from "react-router-dom";
 import FormInput from "./FormInput";
 import SubmitBtn from "./SubmitBtn";
 import { customFetch, formatAsDollars, type Checkout } from "@/utils";
-import { toast } from "@/components/ui/use-toast";
-import { clearCart } from "../features/cart/cartSlice";
+
 import { ReduxStore } from "@/store";
+import { toast } from "@/hooks/use-toast";
+import { clearCart } from "@/featuers/cart/cartSlice";
 
 function CheckoutForm() {
   return <div>CheckoutForm</div>;
