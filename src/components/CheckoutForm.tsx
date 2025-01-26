64ea3e8dc46b8639cd6897ee0c57a9a5dbfbf2d1
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-26 10:28:09 +0430

    adding btn

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -11,6 +11,9 @@ function CheckoutForm() {
   return (
     <Form method="post" className="flex flex-col gap-y-4">
       <h4 className="font-medium text-xl mb-4">Shipping Information</h4>
+      <FormInput label="first name" name="name" type="text" />
+      <FormInput label="address" name="address" type="text" />
+      <SubmitBtn text="Place Your Order" />
     </Form>
   );
 }
