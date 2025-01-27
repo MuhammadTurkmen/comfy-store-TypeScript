380099a3eaec42c23f41bf7813662ff483c30946
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-27 08:31:06 +0430

    adding imports

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,3 +1,15 @@
+import { LoaderFunction, redirect, useLoaderData } from "react-router-dom";
+
+import { customFetch } from "@/utils";
+import {
+  OrdersList,
+  ComplexPaginationContainer,
+  SectionTitle,
+} from "@/components";
+import { ReduxStore } from "@/store";
+import { type OrdersResponse } from "@/utils";
+import { toast } from "@/hooks/use-toast";
+
 function Orders() {
   return <div>Orders</div>;
 }
