fbb9afafe8d163407c1c5d9581d5b6d850e5cab2
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-27 09:05:47 +0430

    adding if

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -13,6 +13,7 @@ import { toast } from "@/hooks/use-toast";
 export const loader =
   (store: ReduxStore): LoaderFunction =>
   async ({ request }): Promise<OrdersResponse | Response | null> => {
+    const user = store.getState().userState.user;
     return null;
   };
 
