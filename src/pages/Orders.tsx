6ceddfdf7c9c9576494416cf52548e0e24033eb5
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-27 09:11:49 +0430

    adding params

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -14,6 +14,16 @@ export const loader =
   (store: ReduxStore): LoaderFunction =>
   async ({ request }): Promise<OrdersResponse | Response | null> => {
     const user = store.getState().userState.user;
+
+    if (!user) {
+      toast({ description: "Please login to continue" });
+      return redirect("/login");
+    }
+
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ]);
+
     return null;
   };
 
