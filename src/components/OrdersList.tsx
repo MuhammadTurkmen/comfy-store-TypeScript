9c32df00a61efc037be632b7f314ea25dabb63a9
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2025-01-28 09:44:21 +0430

    adding import

--- src/components/OrdersList.tsx
+++ src/components/OrdersList.tsx
@@ -1,3 +1,17 @@
+import { useLoaderData } from "react-router-dom";
+
+import { type OrdersResponse } from "@/utils";
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 function OrdersList() {
   return <div>OrdersList</div>;
 }
